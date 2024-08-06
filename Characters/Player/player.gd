24e29f19b6b128f75cb4c338170c8c55229f8857
Dylan <rattledew17@gmail.com>
2024-08-05 17:14:42 -0700

    add: ship can shoot bullets

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -1,15 +1,21 @@
 extends CharacterBody2D
 
-@onready var ship = $Ship
+@onready var marker_2d = $Marker2D
+@onready var bullet_delay_timer = $BulletDelayTimer
 
 const SPEED: float = 600.0
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	var direction = Input.get_axis("move_left", "move_right")
 	velocity.x = direction * SPEED
 	move_and_slide()
 	
-	if Input.is_action_pressed("shoot"):
-		var bullet_instance := preload("res://Common/Projectiles/bullet.tscn").instantiate()
-		bullet_instance.position = ship.position
-		add_child(bullet_instance)
+	if (Input.is_action_pressed("shoot")
+	and bullet_delay_timer.is_stopped()):
+		shoot()
+
+func shoot() -> void:
+	var bullet_instance := preload("res://Common/Projectiles/bullet.tscn").instantiate()
+	bullet_instance.position = marker_2d.get_global_position()
+	get_parent().add_child(bullet_instance)
+	bullet_delay_timer.start()
--- Common/Projectiles/bullet.gd
+++ Common/Projectiles/bullet.gd
@@ -3,8 +3,7 @@ extends Area2D
 @onready var sprite_2d = $Sprite2D
 
 func _physics_process(delta):
-	sprite_2d.position.y -= 10
-
-func _on_area_entered(area):
-	if area.is_in_group("boundary"):
-		print("delete")
+	sprite_2d.position.y -= 5
+	
+func _on_delete_self_timer_timeout():
+	queue_free()
