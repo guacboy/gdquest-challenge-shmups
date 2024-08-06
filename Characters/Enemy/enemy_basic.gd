632b6625a7aa5178f47cae0db4beaea7566222ec
Dylan <rattledew17@gmail.com>
2024-08-05 17:48:34 -0700

    add: bullet collision

--- Characters/Enemy/enemy_basic.gd
+++ Characters/Enemy/enemy_basic.gd
@@ -5,12 +5,19 @@ extends CharacterBody2D
 var new_position_x: int = position.x + 25
 var new_position_y: int = position.y + 25
 
+
 func _physics_process(delta) -> void:
-	var tween := create_tween()
-	tween.tween_property(ship, "position:x", new_position_x, 0.1).set_delay(1.0)
-	tween.tween_property(ship, "position:y", new_position_y, 0.1).set_delay(1.0)
-	tween.finished.connect(
-		func() -> void:
-			new_position_x = position.x + 25
-			new_position_y = position.y + 25
-	)
+	#var tween := create_tween()
+	#tween.tween_property(ship, "position:x", new_position_x, 0.1).set_delay(1.0)
+	#tween.tween_property(ship, "position:y", new_position_y, 0.1).set_delay(1.0)
+	#tween.finished.connect(
+		#func() -> void:
+			#new_position_x = position.x + 25
+			#new_position_y = position.y + 25
+	#)
+	pass
+
+func _on_area_2d_area_entered(area):
+	print("passed")
+	if area.is_in_group("bullet"):
+		queue_free()
--- Common/Projectiles/bullet.gd
+++ Common/Projectiles/bullet.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
-@onready var sprite_2d = $Sprite2D
+@onready var collision_shape_2d = $CollisionShape2D
 
 func _physics_process(delta):
-	sprite_2d.position.y -= 5
+	position.y -= 20
+	
+func _on_area_entered(area):
+	if area.is_in_group("enemy"):
+		queue_free()
 	
 func _on_delete_self_timer_timeout():
 	queue_free()
