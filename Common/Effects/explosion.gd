fd8ffd2428041d67c8e4a7a93ef22513f30596f3
Dylan <rattledew17@gmail.com>
2024-08-07 15:43:57 -0700

    added bullet sound effects

--- Common/Effects/explosion.gd
+++ Common/Effects/explosion.gd
@@ -11,10 +11,9 @@ var explosion_sound_list := [
 	preload("res://Common/Effects/Sound Effects/8bit_expl_short_05.wav"),
 ]
 
-func _process(delta) -> void:
-	if !audio_stream_player_2d.is_playing():
-		audio_stream_player_2d.stream = explosion_sound_list[randi_range(0, len(explosion_sound_list) - 1)]
-		audio_stream_player_2d.play()
+func _ready() -> void:
+	audio_stream_player_2d.stream = explosion_sound_list[randi_range(0, len(explosion_sound_list) - 1)]
+	audio_stream_player_2d.play()
 
 func _on_despawn_timer_timeout() -> void:
 	queue_free()
--- Common/Projectiles/bullet.gd
+++ Common/Projectiles/bullet.gd
@@ -1,5 +1,17 @@
 extends Area2D
 
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
+
+var bullet_sound_list := [
+	preload("res://Common/Projectiles/Sound Effects/bullet_sound_effect_1.mp3"),
+	preload("res://Common/Projectiles/Sound Effects/bullet_sound_effect_2.mp3"),
+	preload("res://Common/Projectiles/Sound Effects/bullet_sound_effect_3.mp3"),
+]
+
+func _ready() -> void:
+	audio_stream_player_2d.stream = bullet_sound_list[randi_range(0, len(bullet_sound_list) - 1)]
+	audio_stream_player_2d.play()
+
 # bullet speed
 func _physics_process(delta):
 	position.y -= 20
