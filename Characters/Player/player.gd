d0c93065c7dfa90e59186c9dce792cb91586b03e
Dylan <rattledew17@gmail.com>
2024-08-07 22:05:44 -0700

    added respawn timer with invincibility frames

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -1,19 +1,26 @@
 extends CharacterBody2D
 
+@onready var ship = $Ship
+@onready var collision_shape_2d = $Area2D/CollisionShape2D
 @onready var marker_2d = $Marker2D
 @onready var bullet_delay_timer = $BulletDelayTimer
+@onready var respawn_timer = $RespawnTimer
+@onready var invincibility_timer = $InvincibilityTimer
 
 const SPEED: float = 600.0
+var player_control: bool = true
 
 func _physics_process(delta) -> void:
+	# movement
 	var direction = Input.get_axis("move_left", "move_right")
 	velocity.x = direction * SPEED
 	move_and_slide()
 	
-	# adds a delay between each bullet; prevents machine gunning
-	if (Input.is_action_pressed("shoot")
-	and bullet_delay_timer.is_stopped()):
-		shoot()
+	if player_control:
+		# adds a delay between each bullet; prevents machine gunning
+		if (Input.is_action_pressed("shoot")
+		and bullet_delay_timer.is_stopped()):
+			shoot()
 
 func shoot() -> void:
 	var bullet_instance := preload("res://Common/Projectiles/bullet.tscn").instantiate()
@@ -30,6 +37,23 @@ func _on_area_2d_area_entered(area):
 		explosion.global_position = global_position
 		explosion.rotation = randi_range(0, 360) # randomizes explosion
 		get_parent().add_child(explosion)
-		queue_free()
 		
-		# respawn with invincibility
+		# disables shooting and "respawns" ship with invincibility frames
+		player_control = false
+		ship.visible = false
+		collision_shape_2d.set_deferred("disabled", true)
+
+		respawn_timer.start()
+
+func _on_respawn_timer_timeout():
+	player_control = true
+	
+	var tween := create_tween()
+	tween.tween_property(ship, "visible", false, 0.1)
+	tween.tween_property(ship, "visible", true, 0.1)
+	tween.set_loops(10)
+	
+	invincibility_timer.start()
+
+func _on_invincibility_timer_timeout():
+	collision_shape_2d.set_deferred("disabled", false)
